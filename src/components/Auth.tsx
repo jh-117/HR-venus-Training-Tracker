049da9a4244072be26f773f116e0c5f36f6cbfa5
jh-117 <limjiahuey2004@gmail.com>
2025-11-25 00:33:09 +0800

    Updated Auth.tsx

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -125,7 +125,7 @@ export function Auth() {
           <span className="font-semibold text-slate-200">Kadosh <span className="text-cyan-400">AI</span></span>
         </div>
          </div>
-         </div>
+         
     </div>
     
         
